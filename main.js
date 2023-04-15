ab345bda6154f59bd1c5ef83131f81c3d2cad26f
Alice <snow.alice@pm.me>
2023-04-15 11:32:05 +0200

    patch main

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ const fetch = require("node-fetch")
 const fs = require('fs')
 
 async function main(){
-    const data = await fetch('https://check.torproject.org/torbulkexitlist')
-    const text = await data.text()
 
-    fs.writeFileSync('ips.txt', text)
+    try {
+        const data = await fetch('https://check.torproject.org/torbulkexitlist')
+        const text = await data.text()
+
+        fs.writeFileSync('ips.txt', text)
+    } catch (error) {
+        
+    }
+    
 }
 
 
